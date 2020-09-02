5b7f8b73a85fb046e0f96e6662b17dc0613e07aa
SAWAMWA <smlmwangi@gmail.com>
2020-09-02 19:01:41 +0300

    landind page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Landing from './Components/landing'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Landing/>
      mazao jjjjjj
     </div>
   );
